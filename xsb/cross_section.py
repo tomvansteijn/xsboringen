30c31439ad4fe7e80970d9a8c19e37a72fd65e75
tomvansteijn <tom.van.steijn@rhdhv.com>
2017-05-31 22:52:55 +0200

    added bare structure cross section

--- xsb/cross_section.py
+++ xsb/cross_section.py
@@ -1,1 +1,29 @@
-from xsb.objects import Borehole
+# -*- coding: utf-8 -*-
+# Tom van Steijn, Royal HaskoningDHV
+
+from xsb.plot import plot_cross_section
+
+
+class Cross_Section(object):
+    def __init__(self, geometry, buffer_distance=None, label=None):
+        self.geometry = geometry
+        self.buffer_distance = buffer_distance
+        self.label = label
+
+    def add_boreholes(self, boreholes, buffer_distance=None):
+        pass
+
+    def add_piezometers(self, piezometers, buffer_distance=None):
+        pass
+
+    def add_points(self, points, buffer_distance=0.):
+        pass
+
+    def add_lines(self, lines):
+        pass
+
+    def add_solids(self, solids):
+        pass
+
+    def plot(self, imagefile):
+        plot_cross_section(self, imagefile)
