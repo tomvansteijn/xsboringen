c339b0606d1de7cf110f2c9ec52bf8ba9b858062
tomvansteijn <tom.van.steijn@rhdhv.com>
2017-06-03 10:48:53 +0200

    added raster and IDF sample methods

--- xsb/rasterfiles.py
+++ xsb/rasterfiles.py
@@ -1,9 +1,41 @@
 # -*- coding: utf-8 -*-
 # Tom van Steijn, Royal HaskoningDHV
 
-import idfpy
+from xsb import idfpy
 
 import rasterio
+import numpy as np
 
-def sample(rasterfile):
-    
+from functools import partial
+import os
+
+
+def sample_raster(rasterfile, coords, bounds_warning):
+    '''sample raster file at coords'''
+    logging.info('reading rasterfile {}'.format(os.path.basename(rasterfile)))
+    with rasterio.open(rasterfile) as src:
+        for value in src.sample(coords, bounds_warning=bounds_warning):
+            if value[0] in src.nodatavals:
+                yield np.nan
+            else:
+                yield float(value[0])
+
+
+def sample_idf(idffile, coords, bounds_warning=True):
+    '''sample IDF file at coords'''
+    logging.info('reading idf file {}'.format(os.path.basename(idffile)))
+    with idfpy.open(idffile) as src:
+        for value in src.sample(coords, bounds_warning=bounds_warning):
+            if value[0] in src.nodatavals:
+                yield np.nan
+            else:
+                yield float(value[0])
+
+
+def sample(gridfile, coords, bounds_warning=False):
+    '''sample gridfile at coords'''
+    if gridfile.lower().endswith('.idf'):
+        sample = partial(sample_idf, bounds_warning=bounds_warning)
+    else:
+        sample = partial(sample_raster, bounds_warning=bounds_warning)
+    return sample(gridfile, coords)
