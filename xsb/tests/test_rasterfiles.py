24cf2744c38fb67f0795b380fbc2923cd7d1c379
tomvansteijn <tom.van.steijn@rhdhv.com>
2017-06-03 10:45:40 +0200

    added test for rasterfiles.py

---
+++ xsb/tests/test_rasterfiles.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Tom van Steijn, Royal HaskoningDHV
+
+from xsb.rasterfiles import sample
+
+
+class TestSample(object):
+    def TestSampleTif(self):
+        rasterfile = 0.
