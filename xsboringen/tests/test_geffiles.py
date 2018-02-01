8589cfd55d1218ef295638ad4ebaff1192779a42
tomvansteijn <tom.van.steijn@rhdhv.com>
2018-02-01 11:05:27 +0100

    tests for read geffiles

--- xsboringen/tests/test_geffiles.py
+++ xsboringen/tests/test_geffiles.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Tom van Steijn, Royal HaskoningDHV
 
-from xsboringen.geffiles import borehole_from_gef, GefFieldNames
+from xsboringen.geffiles import GefCPTFile, GefBoreholeFile
 
 import numpy as np
 
@@ -10,26 +10,40 @@ import os
 
 DATADIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 
+
 class TestBoreholeFromGEF(object):
     def test_read(self):
         geffile = os.path.join(DATADIR, 'DD286.+027_SB_BIK.GEF')
-        repeatedset = {
-            'columninfo',
-            'columnvoid',
-            'measurementtext',
-            'measurementvar',
-            'specimentext',
-            'specimenvar',
-        }
-        fieldnames = GefFieldNames(**{
-            'columnsep': 'columnseparator',
-            'recordsep': 'recordseparator',
-            'code': 'testid',
-            'depth': 'einddiepte',
-            'xy': 'xyid',
-            'z': 'zid',
-            })
-        borehole = borehole_from_gef(geffile, fieldnames, repeatedset)
+        fieldnames = {
+            'columnsep': 'COLUMNSEPARATOR',
+            'recordsep': 'RECORDSEPARATOR',
+            'code': 'TESTID',
+            'xy': 'XYID',
+            'z': 'ZID',
+            }
+        gef = GefBoreholeFile(geffile, fieldnames)
+        borehole = gef.to_borehole()
         assert np.isclose(borehole.z, 14.21)
         borehole.materialize()
-        assert borehole.segments[-2].lithology == 'Zs1g1 GR'
+        assert borehole.segments[-2].lithology == 'Zs1g1'
+        assert borehole.segments[-2].color == ['GR',]
+
+
+class TestCPTFromGEF(object):
+    def test_read(self):
+        geffile = os.path.join(DATADIR, '594781_TG322.+039_SW_KR.GEF')
+        fieldnames = {
+            'columnsep': 'COLUMNSEPARATOR',
+            'recordsep': 'RECORDSEPARATOR',
+            'code': 'TESTID',
+            'xy': 'XYID',
+            'z': 'ZID',
+            }
+        columns = GefColumns(**{
+            'depth': '',
+            'cone_resistance': '',
+            'friction_ratio': '',
+            })
+
+        cpt = gef.to_cpt(geffile, fieldnames, columns)
+
