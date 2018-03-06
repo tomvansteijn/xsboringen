d2438d3bf9154e1d8605206dd5f11fdb458ea9f6
tomvansteijn <tom.van.steijn@rhdhv.com>
2018-03-06 13:34:24 +0100

    added value tuple

--- xsboringen/point.py
+++ xsboringen/point.py
@@ -5,17 +5,19 @@
 
 from xsboringen.mixins import AsDictMixin, CopyMixin
 
+from collections import namedtuple
 from functools import total_ordering
 
 
 @total_ordering
 class Point(AsDictMixin, CopyMixin):
     '''Point class'''
+    Value = namedtuple('Value', ['name', 'value', 'dtype', 'format'])
 
     def __init__(self, code,
             x=None, y=None, z=None,
             top=None, base=None,
-            textfields=None,
+            values=None,
             ):
         self.code = code
 
@@ -26,7 +28,7 @@ class Point(AsDictMixin, CopyMixin):
         self.top = top
         self.base = base
 
-        self.textfields = textfields or []
+        self.values = [self.Value(**v) for v in values or []]
 
     def __repr__(self):
         return ('{s.__class__.__name__:}(code={s.code:})').format(
@@ -34,9 +36,31 @@ class Point(AsDictMixin, CopyMixin):
             )
 
     def __eq__(self, other):
-        return self.z == other.z
+        return self.midlevel == other.midlevel
 
     def __lt__(self, other):
-        return self.z < other.z
+        return self.midlevel < other.midlevel
 
+    @property
+    def geometry(self):
+        '''borehole geometry interface'''
+        return {'type': 'Point', 'coordinates': (self.x, self.y)}
+
+    @property
+    def midlevel(self):
+        if (self.top is None) and (self.base is None):
+            return None
+        elif self.top is None:
+            return self.base
+        elif self.base is None:
+            return self.top
+        else:
+            return (self.top + self.base) / 2.
+
+    def relative_to(self, z):
+        '''return top and base relative to z'''
+        clone = self.copy()
+        clone.top = z - self.top
+        clone.base = z - self.base
+        return clone
 
