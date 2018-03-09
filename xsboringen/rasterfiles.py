7462c59519e847600bd5883263eff6a30a0fff86
tomvansteijn <tom.van.steijn@rhdhv.com>
2018-03-09 18:21:48 +0100

    fix sample rasterfiles

--- xsboringen/rasterfiles.py
+++ xsboringen/rasterfiles.py
@@ -17,32 +17,32 @@ import os
 log = logging.getLogger(os.path.basename(__file__))
 
 
-def sample_raster(rasterfile, coords, bounds_warning):
+def sample_raster(rasterfile, coords):
     '''sample raster file at coords'''
     log.debug('reading rasterfile {}'.format(os.path.basename(rasterfile)))
     with rasterio.open(rasterfile) as src:
-        for value in src.sample(coords, bounds_warning=bounds_warning):
+        for value in src.sample(coords):
             if value[0] in src.nodatavals:
                 yield np.nan
             else:
                 yield float(value[0])
 
 
-def sample_idf(idffile, coords, bounds_warning=True):
+def sample_idf(idffile, coords, boundless=True):
     '''sample IDF file at coords'''
     log.debug('reading idf file {}'.format(os.path.basename(idffile)))
     with idfpy.open(idffile) as src:
-        for value in src.sample(coords, bounds_warning=bounds_warning):
+        for value in src.sample(coords, boundless=boundless):
             if value[0] in src.nodatavals:
                 yield np.nan
             else:
                 yield float(value[0])
 
 
-def sample(gridfile, coords, bounds_warning=False):
+def sample(gridfile, coords, boundless=True):
     '''sample gridfile at coords'''
     if idfpy_imported and gridfile.lower().endswith('.idf'):
-        sample = partial(sample_idf, bounds_warning=bounds_warning)
+        sample = partial(sample_idf, boundless=True)
     else:
-        sample = partial(sample_raster, bounds_warning=bounds_warning)
+        sample = partial(sample_raster)
     return sample(gridfile, coords)
