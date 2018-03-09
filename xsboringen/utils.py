96fa7266a7fd36cc375ad34916d5fb69610aaf05
tomvansteijn <tom.van.steijn@rhdhv.com>
2018-03-09 18:19:57 +0100

    careful_open and careful_glob utils

--- xsboringen/utils.py
+++ xsboringen/utils.py
@@ -2,13 +2,32 @@
 # -*- coding: utf-8 -*-
 # Tom van Steijn, Royal HaskoningDHV
 
+from pathlib import Path
 import glob
 import sys
 import os
 
 
+def splitall(path):
+    allparts = []
+    while True:
+        parts = os.path.split(path)
+        if parts[0] == path:  # sentinel for absolute paths
+            allparts.insert(0, parts[0])
+            break
+        elif parts[1] == path: # sentinel for relative paths
+            allparts.insert(0, parts[1])
+            break
+        else:
+            path = parts[0]
+            allparts.insert(0, parts[1])
+    return allparts
+
+
 def careful_glob(folder, pattern):
-    if not os.path.exists(folder):
+    parts = splitall(folder)
+    basefolder = os.path.join(*[p for p in parts if not p == '*'])
+    if not os.path.exists(basefolder):
         raise ValueError('folder \'{f:}\' does not exist'.format(
             f=folder,
             ))
@@ -21,43 +40,47 @@ def careful_open(filepath, mode):
 
 class CarefulFileOpener(object):
     def __init__(self, filepath, mode):
-        self.filepath = filepath
+        self.filepath = Path(filepath)
         self.mode = mode
         self.handle = None
+
+    def __repr__(self):
+        return ('{s.__class__.__name__:}(file=\'{s.filepath.name:}\', '
+                'mode=\'{s.mode:}\')').format(s=self)
 
     def retry_dialog(self):
         response = None
         while response not in {'r', 'a'}:
             user_input = input(
-                'cannot access \'{fp:}\'\n(R)etry, (A)bort?'.format(
-                fp=filepath,
+                'cannot access \'{fp.name:}\'\n(R)etry, (A)bort?'.format(
+                fp=self.filepath,
                 ))
             try:
                 response = user_input.strip().lower()[0]
             except IndexError:
                 pass
         if response == 'r':
-            self.open()
+            return self.open()
         elif response == 'a':
             sys.exit('aborting')
 
     def open(self):
         try:
-            self.handle = open(self.filepath, self.mode)
-            return self.handle
+            return open(self.filepath, self.mode)
         except PermissionError:
-            self.retry_dialog()
+            return self.retry_dialog()
 
     def close(self):
         if self.handle is not None:
             self.handle.close()
-        self.handle = None
 
     def __enter__(self):
-        return self.open()
+        self.handle = self.open()
+        return self.handle
 
     def __exit__(self, type, value, traceback):
         self.close()
+        self.handle = None
 
 
 
